2c8e2061c6dd16bbba3a050c5e8cf83f91b31357
Joel Júnior <joelviana250@gmail.com>
2025-07-29 15:44:49 -0300

    feat: adicionar menu lateral às versões mobile

--- src/components/AsideMenu.tsx
+++ src/components/AsideMenu.tsx
@@ -1,13 +1,14 @@
+import { IoLogoWhatsapp } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { FiInstagram } from "react-icons/fi";
+import { FaFacebook } from "react-icons/fa";
 export default function AsideMenu() {
   return (
-    <aside className="bg-primary-color h-screen w-[70%] fixed z-1 flex justify-center">
-      <nav className="font-open-sans text-center ">
-        <ul className="flex flex-col gap-4 text-white text-mobile">
+    <aside className="bg-primary-color h-screen w-[70%] fixed z-100 flex justify-center p-10">
+      <nav className="font-open-sans text-center text-white text-mobile-subtitle flex flex-col justify-between xsm:text-mobile">
+        <ul className="flex flex-col gap-4">
           <li>
-            <Link className="" to={"/"}>
-              Home
-            </Link>
+            <Link to={"/"}>Home</Link>
           </li>
           <li>
             <Link to={"/contatos"}>Contatos</Link>
@@ -20,6 +21,31 @@ export default function AsideMenu() {
           </li>
           <li>
             <Link to={"trabalhe-conosco"}>Trabalhe conosco</Link>
+          </li>
+          <li className="mt-5 flex justify-center">
+            <div className="flex text-mobile gap-4 xsm:text-4xl">
+              <a
+                href="http://wa.me/5521969672912"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IoLogoWhatsapp />
+              </a>
+              <a
+                href="https://www.instagram.com/grupoliferio/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiInstagram />
+              </a>
+              <a
+                href="https://web.facebook.com/profile.php?id=61551433251031"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook />
+              </a>
+            </div>
           </li>
         </ul>
       </nav>
