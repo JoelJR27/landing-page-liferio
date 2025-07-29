8c5d1c16392ce1ec9490bc65447c8a744faf0190
Joel Júnior <joelviana250@gmail.com>
2025-07-29 16:38:55 -0300

    feat: finalizar cabeçalho

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,79 @@
 import logoLiferio from "/logo-liferio.jpeg";
-import { IoMdMenu } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { useDeviceWidth } from "../hooks/useDeviceWidth";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoWhatsapp, IoMdMenu } from "react-icons/io";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
 type HeaderProps = {
+  isOpen: boolean;
   handleMenu(): void;
 };
-export default function Header({ handleMenu }: HeaderProps) {
+export default function Header({ isOpen, handleMenu }: HeaderProps) {
+  const deviceWidth = useDeviceWidth();
   return (
-    <header className="bg-primary-color sticky top-0 flex z-10 justify-between px-4">
+    <header className="bg-primary-color sticky top-0 flex z-10 justify-between items-center px-4 lg:justify-around">
       <button
-        className="text-white text-4xl cursor-pointer"
+        className="text-white text-4xl cursor-pointer lg:hidden"
         onClick={handleMenu}
       >
-        <IoMdMenu />
+        {isOpen ? <IoMdCloseCircleOutline /> : <IoMdMenu />}
       </button>
-      <img className="w-[100px] h-[100px]" src={logoLiferio} alt="" />
+      {deviceWidth >= 1024 && (
+        <nav className="font-open-sans text-center text-white font-semibold text-desktop-subtitle flex">
+          <ul className="flex gap-4 xl:gap-10">
+            <li>
+              <Link className="" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contatos"}>Contatos</Link>
+            </li>
+            <li>
+              <Link to={"/servicos"}>Serviços</Link>
+            </li>
+            <li>
+              <Link to={"/sobre"}>Sobre nós</Link>{" "}
+            </li>
+            <li>
+              <Link to={"trabalhe-conosco"}>Trabalhe conosco</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+      <div className="flex text-4xl gap-4">
+        {deviceWidth >= 1024 && (
+          <div className="flex gap-2 items-center text-desktop-subtitle text-white">
+            <a
+              href="http://wa.me/5521969672912"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoWhatsapp />
+            </a>
+            <a
+              href="https://www.instagram.com/grupoliferio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiInstagram />
+            </a>
+            <a
+              href="https://web.facebook.com/profile.php?id=61551433251031"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+          </div>
+        )}
+        <img
+          className="w-[100px] h-[100px]"
+          src={logoLiferio}
+          alt="Logo da Life Rio"
+        />
+      </div>
     </header>
   );
 }
-
-// function AsideMobileMenu() {
-//   return <aside></aside>;
-// }
