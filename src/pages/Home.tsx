4f3ac769f71f051cbbe53bf267c613c46300679b
Joel Júnior <joelviana250@gmail.com>
2025-08-02 13:00:04 -0300

    feat: finalizar página principal 'home'

---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import { Accordions } from "../components/Accordion";
+import AsideMenu from "../components/AsideMenu";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import InfosBanner from "../components/InfosBanner";
+import Location from "../components/Location";
+import Products from "../components/Products";
+import Services from "../components/Services";
+import { useAsideMenu } from "../hooks/useAsideMenu";
+import { useDeviceWidth } from "../hooks/useDeviceWidth";
+
+export default function Home() {
+  const { isOpen, handleOpenMenu } = useAsideMenu();
+  const deviceWidth = useDeviceWidth();
+  return (
+    <>
+      <Header isOpen={isOpen} handleMenu={handleOpenMenu} />
+      {isOpen && deviceWidth < 1024 && (
+        <>
+          <div className="w-full h-screen fixed z-1 bg-black opacity-60"></div>
+          <AsideMenu />
+        </>
+      )}
+      <Hero />
+      <InfosBanner />
+      <Services />
+      <Products />
+      <Accordions.Container>
+        <Accordions.Divider>
+          <Accordions.Accordion summary="Redução dos Riscos Hospitalares">
+            Ao fornecer tratamento em casa, o risco de contrair infecções em
+            ambientes hospitalares é drasticamente reduzido.
+          </Accordions.Accordion>
+          <Accordions.Accordion summary="Proximidade com a Família">
+            O paciente pode ser atendido no conforto de sua própria casa,
+            permitindo que fique perto de seus entes queridos durante o
+            tratamento.
+          </Accordions.Accordion>
+        </Accordions.Divider>
+        <Accordions.Divider>
+          <Accordions.Accordion summary="Garantia de Serviços 24h">
+            Nossa equipe está disponível 24 horas por dia, garantindo
+            atendimento de emergência sempre que necessário.
+          </Accordions.Accordion>
+          <Accordions.Accordion summary="Atendimento Humanizado e Personalizado">
+            Nossos profissionais prestam atenção individualizada a cada
+            paciente, garantindo que seus cuidados sejam atendidos de acordo com
+            as suas necessidades específicas.
+          </Accordions.Accordion>
+        </Accordions.Divider>
+      </Accordions.Container>
+      <Location />
+      <Footer />
+    </>
+  );
+}
