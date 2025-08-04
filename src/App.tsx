c06adc7dd6fd5386d979142e3df13aecd8b12915
Joel Júnior <joelviana250@gmail.com>
2025-08-04 14:49:55 -0300

    terminar aplicação ainda sem animações

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,64 @@
-import AsideMenu from "./components/AsideMenu";
+import React, { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BackToTopButton from "./components/BackToTopButton";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import InfosBanner from "./components/InfosBanner";
-import { useAsideMenu } from "./hooks/useAsideMenu";
-import { useDeviceWidth } from "./hooks/useDeviceWidth";
+import Loading from "./components/Loading";
+const LazyHome = React.lazy(() => import("./pages/Home"));
+const LazyContact = React.lazy(() => import("./pages/Contact"));
+const LazyOurServices = React.lazy(() => import("./pages/OurServices"));
+const LazyAboutUs = React.lazy(() => import("./pages/AboutUs"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <React.Suspense fallback={<Loading />}>
+        <LazyHome />
+      </React.Suspense>
+    ),
+  },
+  {
+    path: "/contatos",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LazyContact />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/servicos",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LazyOurServices />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/sobre",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LazyAboutUs />
+      </Suspense>
+    ),
+  },
+]);
 
 function App() {
-  const { isOpen, handleOpenMenu } = useAsideMenu();
-  const deviceWidth = useDeviceWidth();
-  console.log(window.scrollY);
   return (
-    <main className="relative">
+    <main className="relative font-open-sans">
+      {/* TODO: Buscar sobre quais meta tags são essenciais para utilizar */}
+      {/* TODO: Alterar a largura e altura das imagens do carrossel na versão mobile */}
+      {/* TODO: Fazer um lazy loading para as imagens do carrossel */}
+      {/* TODO: Fazer um lazy loading para o iframe do mapa */}
+      {/* TODO: Criar animações para abertura dos acordeões */}
+      {/* TODO: Criar animações para cabeçalho, abertura do menu lateral */}
+
       <meta
         name="description"
         content="A Life Rio é uma empresa de que visa prestar o serviço de atenção domiciliar de forma segura, qualificada e humanizada, possibilitando a desospitalização e manutenção do cuidado do paciente em domicílio."
       />
       <title>Grupo Liferio</title>
-      <Header isOpen={isOpen} handleMenu={handleOpenMenu} />
+      <RouterProvider router={router} />
       <BackToTopButton />
-      {isOpen && deviceWidth < 1024 && (
-        <>
-          <div className="w-full h-screen fixed z-1 bg-black opacity-60"></div>
-          <AsideMenu />
-        </>
-      )}
-      <Hero />
-      <InfosBanner />
-      <section className="h-screen">teste</section>
     </main>
   );
 }
